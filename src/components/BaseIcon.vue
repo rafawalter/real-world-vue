c7056fbfd0e802a34afb2619235c4b6c83be6917
Rafael Walter <rafawalter@gmail.com>
2019-02-23 18:42:59 -0300

    Global Components

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <svg class="icon" :width="width" :height="height">
+    <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }"></use>
+    // notice +name here
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+function automaticGlobalRegistration() {
+  const requireComponent = require.context(
+    './components',
+    false,
+    /Base[A-Z]\w+\.(vue|js)$/
+  )
+
+  requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName)
+
+    const componentName = upperFirst(
+      camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+    )
+
+    Vue.component(componentName, componentConfig.default || componentConfig)
+  })
+}
+automaticGlobalRegistration()
+
 Vue.config.productionTip = false
 
 new Vue({
